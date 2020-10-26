6d6bc13817b89618cfce3c77608b9348590cb1e1
Maria <user@USERs-MacBook-Pro.local>
2020-10-25 21:13:41 -0700

    added TC-005

--- test/initial.js
+++ test/initial.js
@@ -28,6 +28,11 @@ describe('My Little Hero', function () { //define suite title by passing a strin
       expect(label).toEqual(true);
     });
     
+    it('TC-005 Label for story', function () {
+      const label = $$('.ant-form-item-required')[3].isDisplayed();
+      expect(label).toEqual(true);
+    });
+    
 
     
   });
